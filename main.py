a33adb23fcf970af358a3e359fcfcef75a0b9afa
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-10-16 13:27:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Form, Body
+from pydantic import BaseModel
+import google.generativeai as palm
+from typing import Annotated
+# Configure the Palm library with your API key
+palm.configure(api_key='AIzaSyA-LiZDpliZS0DWEbluHyJTbJ28XPb1d1A')
+
+# Use the palm.list_models function to find available models:
+models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+model = models[0].name
+
+app = FastAPI()
+
+
+@app.get("/")
+def first():
+    return {"message": "Hello, World!"}
+
+
+@app.post("/")
+def generate_text(request_data:Annotated[str, Form()]):
+    p = request_data
+    completion = palm.generate_text(
+        model=model,
+        prompt=p,
+        temperature=0,
+        max_output_tokens=200,
+    )
+
+    return {"generated_text": completion.result}
+
+
+# You can run this FastAPI application using Uvicorn:
+# uvicorn your_module_name:app --host 0.0.0.0 --port 8000
